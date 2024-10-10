fdcf40d199bb6d8006e50f73911abbb8a392226c
Kristex95 <kristex95@gmail.com>
2024-10-11 02:05:56 +0300

    full balance response

--- src/main/java/trade/wayruha/drift/dto/response/BalanceResponse.java
+++ src/main/java/trade/wayruha/drift/dto/response/BalanceResponse.java
@@ -7,4 +7,9 @@ import java.math.BigDecimal;
 @Data
 public class BalanceResponse {
   private BigDecimal balance;
+  private BigDecimal unrealizedPnl;
+  private BigDecimal totalCollateral;
+  private BigDecimal freeCollateral;
+  private BigDecimal totalInitialMargin;
+  private BigDecimal totalMaintenanceMargin;
 }
